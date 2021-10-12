bec020f14577b3d241930eddef0771091de17c8d
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-10-12 23:04:23 +0300

    added a project model

--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Projects(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    proj_image = CloudinaryField('image')
+    repo_link = models.URLField()
+    proj_link = models.URLField()
+    
+
+    class Meta:
+        verbose_name = ("project")
+        verbose_name_plural = ("project")
+
+    def __str__(self):
+        return self.title
+
+    
