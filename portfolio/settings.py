bea88153f90ab7083a4935f9284e5ab881ae9e54
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-10-12 23:02:11 +0300

    configured cloudinary

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'resume'
+    'resume',
+    'cloudinary',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +134,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+cloudinary.config(
+    cloud_name = 'cloud_name',
+    api_key = 'api_key',
+    api_secret = 'api_secret'
+)
