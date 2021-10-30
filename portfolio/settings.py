2ed196cd3a27fc2879df25e37504880eb88a87e1
Binamin-hussein100 <binamin.hassan@student.moringaschool.com>
2021-10-30 11:02:23 +0300

    added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -154,18 +154,3 @@ cloudinary.config(
 )
 
 #add
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-        },
-    },
-}
